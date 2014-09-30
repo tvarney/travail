462d0376fcb5c36e61a8c28be328c470273eb5b9
tvarney <troy.a.varney@gmail.com>
2014-09-30 13:21:17 -0500

    Changed widget methods to be virtual.

--- inc/Widget.hpp
+++ inc/Widget.hpp
@@ -14,15 +14,15 @@ namespace travail {
         virtual int handle(int ch);
         virtual void draw(WINDOW *window) = 0;
         
-        const Dimensions2i & getDim() const;
+        virtual const Dimensions2i & getDim() const;
         
-        const Point2i & getPos() const;
-        void setPos(const Point2i &pos);
-        void setPos(int x, int y);
+        virtual const Point2i & getPos() const;
+        virtual void setPos(const Point2i &pos);
+        virtual void setPos(int x, int y);
         
-        void center(WINDOW *window);
-        void center_h(WINDOW *window);
-        void center_v(WINDOW *window);
+        virtual void center(WINDOW *window);
+        virtual void center_h(WINDOW *window);
+        virtual void center_v(WINDOW *window);
     protected:
         Widget();
         Widget(const Point2i &orig, const Dimensions2i &dim);
