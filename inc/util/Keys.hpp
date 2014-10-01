09fe934f3db052ccb351ec79e70848f1aae28dbb
Troy Varney <troy.a.varney@gmail.com>
2014-10-01 14:21:59 -0500

    Added cntrl and meta constexpr functions for modifying character constants.

---
+++ inc/util/Keys.hpp
@@ -0,0 +1,38 @@
+
+#ifndef TRAVAIL_UTIL_KEYS_HPP
+#define TRAVAIL_UTIL_KEYS_HPP
+
+namespace travail {
+    /**
+     * \brief Modify the character given to be the result of a control
+     * sequence.
+     * 
+     * This function modifies the character given as if it was returned by
+     * ncurses \c getch() when the user pressed the letter \c ch represents
+     * while holding the control key. This is only guaranteed to work
+     * accurately for values of \c ch for which \c std::isalpha(ch) would
+     * return true.
+     * 
+     * \arg \c ch The character to modify
+     * \return The character with only the first 5 bits allowed
+     */
+    constexpr char cntrl(char ch) {
+        return ch & 0x1F;
+    }
+    /**
+     * \brief Modify the character given to be the result of a meta sequence.
+     * 
+     * This function modifies the character given as if it was returned by
+     * ncurses \c getch when the user pressed the letter \c ch represents while
+     * holding the meta/alt key. This is only guaranteed to work accurately for
+     * values of \c ch for which std::isalpha(ch) would return true.
+     * 
+     * \arg \c ch The character to modify
+     * \return The character with the high bit set
+     */
+    constexpr char meta(char ch) {
+        return ch | 0x80;
+    }
+}
+
+#endif
