7368991af6815c670abdef2a55c37172f49e0228
tvarney <troy.a.varney@gmail.com>
2014-10-02 12:09:47 -0500

    Updated New Game scene to use the Row and Column classes

--- src/NewGameScene.cpp
+++ src/NewGameScene.cpp
@@ -1,6 +1,8 @@
 
 #include "ui/scenes/NewGameScene.hpp"
 
+#include "ui/Column.hpp"
+#include "ui/Row.hpp"
 #include "ui/SceneStack.hpp"
 #include "util/Curses.hpp"
 #include "util/Keys.hpp"
@@ -14,7 +16,8 @@ NewGameScene::NewGameScene() :
     m_Name(0,1,"Name:",A_BOLD,40),
     m_Age(0,2,"Age:",A_BOLD,4),
     m_Height(10,2,"Height:",A_BOLD,10),
-    m_Weight(29,2,"Weigth:",A_BOLD,10)
+    m_Weight(29,2,"Weigth:",A_BOLD,10),
+    m_LabelNext(76,21, "Next", A_BOLD)
 {
     //m_TextName.setMaxChars(40);
     //m_TextAge.setMaxChars(4);
@@ -24,26 +27,38 @@ NewGameScene::NewGameScene() :
 NewGameScene::~NewGameScene() { }
 
 void NewGameScene::run() {
+    Column column;
+    Row row1;
+    row1.add(m_Age);
+    row1.add(m_Height);
+    row1.add(m_Weight);
+    
+    column.add(m_Name);
+    column.add(row1);
+    column.add(m_LabelNext);
+    
     m_LabelTitle.draw();
     m_LabelHelp1.draw();
     m_LabelHelp2.draw();
-    m_Name.draw();
-    m_Age.draw();
-    m_Height.draw();
-    m_Weight.draw();
+    
+    column.draw();
     
     wrefresh(stdscr);
     
-    int line = 0, row = 0;
     int ch;
     while((ch = getch()) != travail::cntrl('q')) {
-        switch(ch) {
-        case '\t':
-        case KEY_BTAB:
-        case KEY_UP:
-        case KEY_DOWN:
+        switch(column.handle(ch)) {
+        case KEY_ENTER:
+            if(&column.getFocused() == &m_LabelNext) {
+                m_Stack->pop();
+                return;
+            }
+            break;
+        case 0:
+            wrefresh(stdscr);
             break;
         }
     }
+    //TODO: Move back to MainMenu if the loop exits
     m_Stack->pop();
 }
