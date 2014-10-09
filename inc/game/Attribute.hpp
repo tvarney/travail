9b411806f15e13e013232ffe7288d12ad96e06ea
tvarney <troy.a.varney@gmail.com>
2014-10-09 14:16:19 -0500

    Changed Attribute to be a class, added lots of methods, rearranged the enum, etc.

--- inc/game/Attribute.hpp
+++ inc/game/Attribute.hpp
@@ -3,37 +3,73 @@
 #define TRAVAIL_ATTRIBUTE_HPP
 
 #include <stdint.h>
+#include <limits>
 
 namespace travail {
-    struct Attribute {
+    class Attribute {
+    public: //< Enumeration
         enum Id {
-            STR = 0,
-            Strength = 0,
-            DEX = 1,
-            Dexterity = 1,
-            AGL = 2,
-            Agility = 2,
-            CON = 3,
-            Constitution = 3,
-            INT = 4,
-            Intelligence = 4,
-            WIS = 5,
-            Wisdom = 5,
-            CHA = 6,
-            Charisma = 6,
+            HitPoints,
+            FatiguePoints,
+            Strength,
+            Dexterity,
+            Agility,
+            Constitution,
+            Intelligence,
+            Wisdom,
+            Charisma,
             
-            AttributeNumber
+            HP  = HitPoints,
+            FP  = FatiguePoints,
+            STR = Strength,
+            DEX = Dexterity,
+            AGL = Agility,
+            CON = Constitution,
+            INT = Intelligence,
+            WIS = Wisdom,
+            CHA = Charisma,
+            
+            Length
         };
         
-        Attribute(uint32_t value);
-        Attribute(uint32_t current, uint32_t max);
-        ~Attribute();
+        enum Status {
+            Zero,
+            Bad,
+            Poor,
+            Damaged,
+            Okay,
+            Normal
+        };
         
-        static const char * Name(Attribute::Id id);
+    public: //< Static API
+        static const char * ShortName(Attribute::Id id);
         static const char * LongName(Attribute::Id id);
         
-        uint32_t current;
-        uint32_t max;
+    public: // API
+        Attribute();
+        Attribute(int32_t value);
+        Attribute(int32_t base, int32_t max, int32_t current);
+        Attribute(const Attribute &source);
+        ~Attribute();
+        
+        Attribute & operator=(const Attribute &rhs);
+        
+        void heal(int32_t amount = std::numeric_limits<int32_t>::max());
+        void damage(int32_t amount);
+        
+        void incMax(int32_t amount = 1);
+        void decMax(int32_t amount = 1);
+        
+        void incLevel(int32_t amount = 1);
+        void decLevel(int32_t amount = 1);
+        
+        Attribute::Status status() const;
+        
+        int32_t base() const;
+        int32_t maximum() const;
+        int32_t current() const;
+    protected: //< Data
+        int32_t m_Base, m_Max, m_Current;
     };
 }
 
--- src/Attribute.cpp
+++ src/Attribute.cpp
@@ -3,51 +3,87 @@
 
 using namespace travail;
 
-Attribute::Attribute(uint32_t value) :
-    current(value), max(value)
+// Static API
+static const char * _g_Attr_LongName[] = {
+    "Hit Points", "Fatigue Points", "Strength", "Dexterity", "Agility",
+    "Constitution", "Intelligence", "Wisdom", "Charisma"
+};
+static const char * _g_Attr_ShortName[] = {
+    "HP", "FP", "Str", "Dex", "Agl", "Con", "Int", "Wis", "Cha"
+};
+
+const char * Attribute::ShortName(Attribute::Id id) {
+    if(id >= Attribute::Length || id < 0) {
+        return "???";
+    }
+    return _g_Attr_ShortName[id];
+}
+const char * Attribute::LongName(Attribute::Id id) {
+    if(id >= Attribute::Length || id < 0) {
+        return "???";
+    }
+    return _g_Attr_LongName[id];
+}
+
+// API
+
+Attribute::Attribute() :
+    m_Base(10), m_Max(10), m_Current(10)
 { }
-Attribute::Attribute(uint32_t current, uint32_t max) :
-    current(current), max(max)
+Attribute::Attribute(int32_t value) :
+    m_Base(value), m_Max(value), m_Current(value)
+{ }
+Attribute::Attribute(int32_t base, int32_t max, int32_t current) :
+    m_Base(base), m_Max(max), m_Current(current)
+{ }
+Attribute::Attribute(const Attribute &source) :
+    m_Base(source.m_Base), m_Max(source.m_Max), m_Current(source.m_Current)
 { }
 Attribute::~Attribute() { }
 
-const char * Attribute::Name(Attribute::Id id) {
-    switch(id) {
-    case STR:
-        return "STR";
-    case DEX:
-        return "DEX";
-    case AGL:
-        return "AGL";
-    case CON:
-        return "CON";
-    case INT:
-        return "INT";
-    case WIS:
-        return "WIS";
-    case CHA:
-        return "CHA";
-    default:
-        return "???";
+Attribute & Attribute::operator=(const Attribute &rhs) {
+    m_Base    = rhs.m_Base;
+    m_Max     = rhs.m_Max;
+    m_Current = rhs.m_Current;
+    return *this;
+}
+
+void Attribute::heal(int32_t amount) {
+    if(amount > m_Max - m_Current) {
+        m_Current = m_Max;
+    }else {
+        m_Current += amount;
     }
 }
-const char * Attribute::LongName(Attribute::Id id) {
-    switch(id) {
-    case STR:
-        return "Strength";
-    case DEX:
-        return "Dexterity";
-    case AGL:
-        return "Agility";
-    case CON:
-        return "Constitution";
-    case INT:
-        return "Intelligence";
-    case WIS:
-        return "Wisdom";
-    case CHA:
-        return "Charisma";
-    default:
-        return "???";
+
+void Attribute::damage(int32_t amount) {
+    m_Current -= amount;
+}
+
+void Attribute::incMax(int32_t amount) {
+    m_Max += amount;
+}
+void Attribute::decMax(int32_t amount) {
+    m_Max -= amount;
+    if(m_Current > m_Max) {
+        m_Current = m_Max;
     }
+}
+
+void Attribute::incLevel(int32_t amount) {
+    m_Base += amount;
+    m_Max += amount;
+}
+void Attribute::decLevel(int32_t amount) {
+    m_Base -= amount;
+    m_Max -= amount;
+}
+
+Attribute::Status Attribute::status() const {
+    if(m_Current <= 0) return Attribute::Zero;
+    if(m_Current <= (m_Max >> 3)) return Attribute::Bad;
+    if(m_Current <= (m_Max >> 2)) return Attribute::Poor;
+    if(m_Current <= (m_Max >> 1)) return Attribute::Damaged;
+    if(m_Current <= ((m_Max >> 1) + (m_Max >> 2))) return Attribute::Okay;
+    return Attribute::Normal;
 }
