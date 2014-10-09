84c6ae42c024ac03f17b6ff718ebc9d36ad85231
tvarney <troy.a.varney@gmail.com>
2014-10-09 14:15:24 -0500

    Main method now calls travail::init_curses() instead of doing it manually

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,7 @@
 #include "util/Curses.hpp"
 
 int main(int argc, char **argv) {
-    initscr();
-    if(has_colors()) {
-        start_color();
-        init_pair(1, COLOR_RED, COLOR_BLACK);
-        init_pair(2, COLOR_GREEN, COLOR_BLACK);
-        init_pair(3, COLOR_BLUE, COLOR_BLACK);
-    }
-    
-    raw();
-    noecho();
-    keypad(stdscr, 1);
-    ESCDELAY = 50;
+    travail::init_curses();
     
     travail::SceneStack stack;
     travail::MainMenu mainmenu;
